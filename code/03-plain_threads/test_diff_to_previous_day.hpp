accf1d02267bd8040785c6e93d1a4250697c40a8
steinbac <steinbac@mpi-cbg.de>
2016-03-03 21:09:05 +0100

    started third lesson

---
+++ code/03-plain_threads/test_diff_to_previous_day.hpp
@@ -0,0 +1,17 @@
+#define test_accessor
+#include <cmath>
+
+#include "gtest/gtest.h"
+#include "iot.hpp"
+
+
+TEST(access_works,in_first_room) {
+  EXPECT_GT(iot::active_devices(0,0),0);
+  
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
