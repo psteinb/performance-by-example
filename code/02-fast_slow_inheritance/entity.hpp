4c61ed779f1961990b1fdbff8ca42739edfc13d4
steinbac <steinbac@mpi-cbg.de>
2016-03-07 22:43:25 +0100

    code for lesson 2 looks promising

---
+++ code/02-fast_slow_inheritance/entity.hpp
@@ -0,0 +1,85 @@
+#ifndef __ENTITY_HPP__
+#define __ENTITY_HPP__
+#include <string>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include "info.hpp"
+
+namespace iot {
+
+  struct temperature_view {
+
+    int value;
+    std::string html_start;
+    std::string val;
+    std::string html_end;
+
+    temperature_view(int _data):
+      value(_data),
+      html_start("<em>"),
+      val(std::to_string(_data)),
+      html_end(" K</em>"){}
+
+    int prepare() const {
+      
+      // std::this_thread::sleep_for(dur);
+      return val.size();
+    }
+  };
+
+  struct power_view {
+
+    float value;
+    std::string html_start;
+    std::string val;
+    std::string html_end;
+
+    power_view(float _data):
+      value(_data),
+      html_start("<em>"),
+      val(std::to_string(_data)),
+      html_end(" kWh</em>"){}
+
+    int prepare() const {
+      
+      // std::this_thread::sleep_for(dur);
+      return val.size();
+    }
+    
+  };
+
+  
+  struct entity {
+
+    temperature_view temp;
+    power_view pow;
+    
+
+    entity():
+      temp(0),
+      pow(0)
+    {
+      
+    }
+    
+    entity(const device_t& _dev):
+      temp(_dev.temperature_kelvin),
+      pow(_dev.power_consumption)
+    {
+      
+    }
+
+    temperature_view const * temperature() const {
+      return &temp;
+    }
+
+    power_view const * power() const {
+      return &pow;
+    }
+
+  };
+  
+}
+
+#endif
--- code/02-fast_slow_inheritance/test_runtime_device_display.cpp
+++ code/02-fast_slow_inheritance/test_runtime_device_display.cpp
@@ -3,18 +3,75 @@
 #include <chrono>
 #include <vector>
 #include <random>
+#include <iostream>
 
 #include "gtest/gtest.h"
+#include "entity.hpp"
 #include "iot.hpp"
 
 using fp_microseconds_t = std::chrono::duration<double, std::chrono::microseconds::period>;
 using fp_nanoseconds_t = std::chrono::duration<double, std::chrono::nanoseconds::period>;
 static int repeats = 10;
 
+TEST(Call_Devices, runtime_original) {
 
-TEST(Call_Devices, runtime_real) {
+  iot::reset_building(8,16);
+
+  EXPECT_LE(iot::size(),8*16);
   
-  EXPECT_GT(iot::devices_.size(),0);
+  std::vector<iot::entity> current_devices;
+  current_devices.reserve(iot::size());
+
+  for( const iot::device_t& d : iot::device_info )
+    current_devices.push_back(iot::entity(d));
+
+  std::size_t tmp_size = 0;
+  std::size_t pow_size = 0;
+  
+  for(int r = 0;r<repeats;++r){
+    for( const iot::entity& e : current_devices )
+      tmp_size += e.temperature()->prepare();
+
+    for( const iot::entity& e : current_devices )
+      pow_size += e.power()->prepare();
+  }
+  
+  EXPECT_GT(pow_size,0.);
+  EXPECT_GT(tmp_size,0.);
+}
+
+
+TEST(Call_Devices, runtime_opt) {
+
+  iot::reset_building(8,16);
+
+
+  EXPECT_LE(iot::size(),8*16);
+  
+  std::vector<int> current_power_width;
+  current_power_width.reserve(iot::size());
+  std::vector<int> current_temp_width;
+  current_temp_width.reserve(iot::size());
+
+  size_t count = 0;
+  for( const iot::device_t& d : iot::device_info ){
+    current_power_width.push_back(std::to_string(d.power_consumption).size());
+    current_temp_width.push_back(std::to_string(d.temperature_kelvin).size());
+  }
+
+  std::size_t tmp_size = 0;
+  std::size_t pow_size = 0;
+  
+  for(int r = 0;r<repeats;++r){
+    for( size_t i = 0;i<iot::size();++i )
+      pow_size += current_power_width[i];
+
+    for( size_t i = 0;i<iot::size();++i )
+      tmp_size += current_temp_width[i];
+  }
+  
+  EXPECT_GT(pow_size,0.);
+  EXPECT_GT(tmp_size,0.);
   
 }
 
