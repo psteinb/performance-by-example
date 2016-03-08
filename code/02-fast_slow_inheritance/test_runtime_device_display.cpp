e270a4ab2fdeabf346097dc5e8cb1f1ef4923161
steinbac <steinbac@mpi-cbg.de>
2016-03-08 23:35:01 +0100

    removed obsolete variable

--- code/02-fast_slow_inheritance/test_runtime_device_display.cpp
+++ code/02-fast_slow_inheritance/test_runtime_device_display.cpp
@@ -1,4 +1,4 @@
-#define test_accessor
+#define test_runtime_device_display
 #include <cmath>
 #include <chrono>
 #include <vector>
@@ -6,54 +6,46 @@
 #include <iostream>
 
 #include "gtest/gtest.h"
-#include "entity.hpp"
-#include "iot.hpp"
+#include "setup_fixture.hpp"
 
-using fp_microseconds_t = std::chrono::duration<double, std::chrono::microseconds::period>;
-using fp_nanoseconds_t = std::chrono::duration<double, std::chrono::nanoseconds::period>;
-static int repeats = 10;
+#include "render_entity.hpp"
 
-TEST(Call_Devices, runtime_original) {
 
-  iot::reset_building(8,16);
+TEST_F(iot_setup, runtime_original) {
 
-  EXPECT_LE(iot::size(),8*16);
-  
-  std::vector<iot::entity> current_devices;
+  std::vector<iot::render_entity> current_devices;
   current_devices.reserve(iot::size());
 
   for( const iot::device_t& d : iot::device_info )
-    current_devices.push_back(iot::entity(d));
+    current_devices.push_back(iot::render_entity(d));
 
   std::size_t tmp_size = 0;
   std::size_t pow_size = 0;
   
+  auto start = std::chrono::high_resolution_clock::now();
   for(int r = 0;r<repeats;++r){
-    for( const iot::entity& e : current_devices )
+    for( const iot::render_entity& e : current_devices )
       tmp_size += e.temperature()->prepare();
 
-    for( const iot::entity& e : current_devices )
+    for( const iot::render_entity& e : current_devices )
       pow_size += e.power()->prepare();
   }
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double, std::milli> elapsed = end-start;
+  std::cout << "original: " << elapsed.count() << "ms\n";
   
   EXPECT_GT(pow_size,0.);
   EXPECT_GT(tmp_size,0.);
 }
 
 
-TEST(Call_Devices, runtime_opt) {
+TEST_F(iot_setup, runtime_opt) {
 
-  iot::reset_building(8,16);
-
-
-  EXPECT_LE(iot::size(),8*16);
-  
   std::vector<int> current_power_width;
   current_power_width.reserve(iot::size());
   std::vector<int> current_temp_width;
   current_temp_width.reserve(iot::size());
 
-  size_t count = 0;
   for( const iot::device_t& d : iot::device_info ){
     current_power_width.push_back(std::to_string(d.power_consumption).size());
     current_temp_width.push_back(std::to_string(d.temperature_kelvin).size());
@@ -61,6 +53,8 @@ TEST(Call_Devices, runtime_opt) {
 
   std::size_t tmp_size = 0;
   std::size_t pow_size = 0;
+
+  auto start = std::chrono::high_resolution_clock::now();
   
   for(int r = 0;r<repeats;++r){
     for( size_t i = 0;i<iot::size();++i )
@@ -69,6 +63,10 @@ TEST(Call_Devices, runtime_opt) {
     for( size_t i = 0;i<iot::size();++i )
       tmp_size += current_temp_width[i];
   }
+  
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double, std::milli> elapsed = end-start;
+  std::cout << "optimised: " << elapsed.count() << "ms\n";
   
   EXPECT_GT(pow_size,0.);
   EXPECT_GT(tmp_size,0.);
