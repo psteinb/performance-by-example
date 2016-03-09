88b00f8d1f043a5d14de408f0775c67fa493e90e
Peter Steinbach <steinbac@mpi-cbg.de>
2016-03-09 18:41:19 +0100

    still computes the wrong result

--- code/04-threads-for-the-rest-of-us/test_parallel_reduce.cpp
+++ code/04-threads-for-the-rest-of-us/test_parallel_reduce.cpp
@@ -27,7 +27,7 @@ double sum_iterators(it _begin, it _end){
 }
 
 
-double sum_power(const std::vector<device_t>& _data){
+double serial_power_sum(const std::vector<device_t>& _data){
 
   double result = 0;
 
@@ -45,23 +45,54 @@ struct power_sum{
     result(0){}
   
   power_sum(power_sum& _rhs, tbb::split ):
-    result(0.)
+    result(0)
   {}
 
   ~power_sum(){}
 
   void operator()(const tbb::blocked_range<device_t*>& range) {
     
-    double local = result;
+    double local = 0;
     
     for( device_t* i=range.begin(); i!=range.end(); ++i )
       local += i->power_consumption;
 
-    result = local;
+    result += local;
 
   }
 
   void join(power_sum& _rhs){ result += _rhs.result; }
+};
+
+struct index_based_sum{
+
+  double result;
+  std::vector<iot::device_t>* data;
+
+  index_based_sum(std::vector<iot::device_t>* _data):
+    result(0),
+    data(_data)
+  {}
+  
+  index_based_sum(index_based_sum& _rhs, tbb::split ):
+    result(0),
+    data(_rhs.data)
+  {}
+
+  ~index_based_sum(){}
+
+  void operator()(const tbb::blocked_range<size_t>& range) {
+    
+    double local = 0;
+    
+    for( size_t i=range.begin(); i!=range.end(); ++i )
+      local += (*data)[i].power_consumption;
+
+    result += local;
+
+  }
+
+  void join(index_based_sum& _rhs){ result += _rhs.result; data = _rhs.data; }
 };
 
 
@@ -70,12 +101,12 @@ TEST(power_total,non_zero_device_infos_available) {
   
 }
 
-TEST(power_total,sum_seq) {
+TEST(power_total,serial) {
 
   double sum = 0;
   auto start_t = std::chrono::high_resolution_clock::now();
   for(int i = 0;i<repeats;++i){
-    sum = sum_power(iot::device_info);
+    sum = serial_power_sum(iot::device_info);
     
   }
   auto end_t = std::chrono::high_resolution_clock::now();
@@ -107,12 +138,10 @@ TEST(power_total,parallel_same_as_seq) {
 
   std::vector<device_t> dinfo = iot::device_info;
 
+  const size_t items_per_thread = (32 + std::thread::hardware_concurrency() - 1)/std::thread::hardware_concurrency();
+  tbb::blocked_range<size_t> brange(0,32,items_per_thread);
 
-  
-  tbb::blocked_range<device_t*> brange(&dinfo[0],
-				       &dinfo[0] + 128);
-
-  power_sum functor;
+  index_based_sum functor(&dinfo);
 
   auto start_t = std::chrono::high_resolution_clock::now();
   for(int i = 0;i<repeats;++i){
@@ -122,8 +151,11 @@ TEST(power_total,parallel_same_as_seq) {
   EXPECT_GT(functor.result,0.);
 
   double serial_sum = sum_iterators(&dinfo[0],
-				    &dinfo[0] + 128);
-  ASSERT_EQ((int)functor.result,(int)serial_sum);
+				    &dinfo[0] + 32);
+
+  int iserial = serial_sum;
+  int iparallel = functor.result;
+  ASSERT_EQ(iserial,iparallel);
   EXPECT_FLOAT_EQ(functor.result,serial_sum);
 }
 
