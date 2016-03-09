e2c5c3e3ec810fc9ae7394625d2e6544fafe4232
Peter Steinbach <steinbac@mpi-cbg.de>
2016-03-09 23:58:47 +0100

    imported headers in lesson 04

---
+++ code/04-threads-for-the-rest-of-us/iot.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include <vector>
+#include <random>
+
+namespace iot {
+
+  static int n_levels_ = 64;
+  static int n_rooms_ = 1 << 18;
+
+  
+  static int n_rooms(){
+    return n_rooms_;
+  }
+
+  static int n_levels(){
+    return n_levels_;
+  }
+
+  static void reset_building(int level_number, int room_number){
+    n_levels_ = level_number;
+    n_rooms_ = room_number;
+  }
+  
+  static int size(){
+    return n_levels_*n_rooms_;
+  }
+  
+  static std::vector<int> fill_devices(){
+    std::srand(size());
+    
+    std::vector<int> value(size());
+    for (int & i : value)
+      i = (std::rand()/RAND_MAX) > .5 ? 1 : 0;
+    return value;
+  }
+  
+  static std::vector<int> devices_ = fill_devices();
+
+  static int active_devices(int level_number, int room_number){
+    return devices_[(level_number*n_rooms())+room_number];
+  }
+  
+}
